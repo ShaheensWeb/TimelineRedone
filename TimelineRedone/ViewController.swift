02db67b6ed5ecbbc1c1b0304521a0c5eb6ad2d60
Shaheen Ghazazani <shaheen.ghazazani@cenx.com>
2016-06-05 14:35:39 -0400

    Initial Commit

---
+++ TimelineRedone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimelineRedone
+//
+//  Created by Shaheen Ghazazani on 2016-06-05.
+//  Copyright © 2016 Sghazazani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
